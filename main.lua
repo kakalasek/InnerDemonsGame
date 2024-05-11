b929973d3a845f77f0f7bc0de9024ed4c50af2fb
Kakalasek <honziccekkj@seznam.cz>
2024-05-11 12:49:53 +0200

    comments

--- main.lua
+++ main.lua
@@ -1,24 +1,27 @@
+-- special love function
+-- gets called only once, used for start configurations
 function love.load()
-    Object = require "lib/classic"
-    camera = require "lib/camera"
+    Object = require "lib/classic"  -- library for easy implementation on OOP in Lua
+    camera = require "lib/camera"   -- library for implementation of camera in the game
 
-    cam = camera()
+    cam = camera()  -- creating the camera object (looks like OOP right? Yeah .. it is actually just a function which return an object .. so basically OOP .. and it works! No need for heavy, complicated constructs)
 
-    anim8 = require "lib/anim8"
-    sti = require "lib/sti"
-    wf = require "lib/windfield"
+    anim8 = require "lib/anim8" -- library for animation related stuff
+    sti = require "lib/sti" -- library for map related stuff (loading, rendering, etc. )
+    wf = require "lib/windfield" -- library for implementing gravity and colliders into the game
 
-    love.graphics.setDefaultFilter("nearest", "nearest")
+    love.graphics.setDefaultFilter("nearest", "nearest")    -- does something important I presume .. althought I dont know what .. Imma leave it here tho
 
-    world = wf.newWorld(0, 100)
+    world = wf.newWorld(0, 100) -- creates a ned windfield world and sets the strength of its gravity
 
-    gameMap = sti('maps/FourthMap.lua')
+    gameMap = sti('maps/SixthMap.lua')  -- loads the game map (it is created using tiled, which can export the map into lua code, so it can be used here)
 
-    require "player"
+    require "player"    -- loads our player code onto here .. it is important to require it right here, because it uses the libraries above
 
-    player = Player()
+    player = Player()   -- creates the player object
 
-    solidObjects = {}
+    -- will load the colliders created in tiled into our real map
+    solidObjects = {}   -- 
     if gameMap.layers["SolidObjects"] then
         for i, obj in pairs(gameMap.layers["SolidObjects"].objects) do
             local solidObject = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
@@ -29,12 +32,16 @@ function love.load()
 
 end
 
+-- special love function which is called repeatedly
+-- updates everything before it gets drawn
 function love.update(dt)
     world:update(dt)
     player:update(dt)
     cam:lookAt(player.x, player.y)
 end
 
+-- special love function which is called repeatedly
+-- draws everything on the screen
 function love.draw()
     cam:attach()
         gameMap:drawLayer(gameMap.layers['Background'])
--- player.lua
+++ player.lua
@@ -1,31 +1,43 @@
-Player = Object:extend();
+-- Player object
+-- NOTE: All library objects are defined in a different file (world, anim8, etc. )
 
-function Player:new()
+Player = Object:extend(); -- creates the player object
+
+
+function Player:new() -- constructor for the Player object
+
+    -- defining width and height of one animation frame .. and defining the scale
     self.animWidth = 12
     self.animHeight = 18
     self.scale = 3
 
-    self.collider = world:newRectangleCollider(100, 360, self.animWidth * self.scale, self.animHeight * self.scale)
-    self.collider:setFixedRotation(true)
+    -- creating a collider for the player
+    self.collider = world:newRectangleCollider(100, 360, self.animWidth * self.scale, self.animHeight * self.scale) -- 'world' is an object created with the windfield library
+                                                                                                                    -- this object adds gravity to the game
+    self.collider:setFixedRotation(true) -- since we have a 2D game, we dont want our collider to rotate
 
-    self.speed = 200
+    self.speed = 200 -- sets the speed of our player
 
-    self.spriteSheet = love.graphics.newImage('textures/player-sheet.png')
+    self.spriteSheet = love.graphics.newImage('textures/player-sheet.png')  -- load our spirtesheet into a property AKA variable AKA to hell with OOP
 
-    self.grid = anim8.newGrid(self.animWidth, self.animHeight, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
-
-    self.animations = {}
-    self.animations.left = anim8.newAnimation( self.grid('1-4', 2), 0.2)
+    self.grid = anim8.newGrid(self.animWidth, self.animHeight, self.spriteSheet:getWidth(), self.spriteSheet:getHeight()) -- creates a grid for our player animation
+                                                                                                                          -- we need to define the width and height of our animation and the width and height of our spritesheet
+                                                                                                                          -- we are using the anim8 library to do that
+    -- creating the different animations                                                                                                                    
+    self.animations = {} -- we will store the animations in a table using different key to access each set
+    self.animations.left = anim8.newAnimation( self.grid('1-4', 2), 0.2) -- first attribute are the frames to be used for the animation (we get them from our grid), second argument sets the duration of the animatiion
     self.animations.right = anim8.newAnimation( self.grid('1-4', 3), 0.2)
 
-    self.anim = self.animations.right
+    self.anim = self.animations.right -- sets the default animation to 'right' (basically means the player will look right at the start of the game)
 end
 
+-- update function for the Player object
 function Player:update(dt)
-    local isMoving = false
+    local isMoving = false  -- so the player animation will stop after the player stops moving
 
-    local vx = 0
+    local vx = 0 -- so the player velocity in the x direction will reset to 0 if the player stops moving
 
+    -- a simple else if to check which direction the player is moving
     if love.keyboard.isDown("a") then
         vx = - self.speed
         self.anim = self.animations.left
@@ -36,16 +48,22 @@ function Player:update(dt)
         isMoving = true
     end
 
-    self.collider:setLinearVelocity(vx, 200)
+    self.collider:setLinearVelocity(vx, 200) -- since the player is actually locked in the collider, we need to move the collider, not the player
+                                             -- first argument is the velocity in the x direction the second one in the y direction (so basically how fast will our player fall)
 
-    if not isMoving then self.anim:gotoFrame(2) end
+    if not isMoving then self.anim:gotoFrame(2) end -- if our player is not moving, we want to display this frame of our animation
+                                                    -- here the beaty of Lua .. everything can be written on one line .. not that we want that everytime .. but it can save space and make the code more readable
 
+    -- Now we set our cords according to our collider .. we wil use them later in the draw function 
     self.x = self.collider:getX()
     self.y = self.collider:getY()
 
+    -- we update our animation
     self.anim:update(dt)
 end
 
+-- draw function for our player object
 function Player:draw()
-    self.anim:draw(self.spriteSheet, self.x, self.y, nil, self.scale, nil, self.animWidth/2, self.animHeight/2)
+    self.anim:draw(self.spriteSheet, self.x, self.y, nil, self.scale, nil, self.animWidth/2, self.animHeight/2) -- will draw our player
+                                                                                                                -- the random nil values are there, because there are positional arguments, which we dont want to set
 end
